2bb6c3e55c04f622e463d698ed16e88749d89c79
leoBNDR-BR <leonardobender94@gmail.com>
2025-09-29 22:12:55 -0300

    commit 1 real

--- aula3.js
+++ aula3.js
@@ -1,6 +1,11 @@
-console.log('Hello World!');
+console.log('Hello World!'); //tipo string
 console.log("Hello World 2!");
 console.log(`Hello World 3!`);
 
-console.log(123456);
+console.log(123456); //tipo number
 console.log(12.3456);
+
+console.log(true); //tipo boolean
+console.log(false);
+
+consol
