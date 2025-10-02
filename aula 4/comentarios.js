acbee777e97167ed79f343b134e6b64cd20e3b50
leoBNDR-BR <leonardobender94@gmail.com>
2025-10-01 21:59:57 -0300

    commit 10/01

---
+++ aula 4/comentarios.js
@@ -0,0 +1,17 @@
+// E escrever um comentário
+console.log("Olá, mundo!"); // Isso é um comentário de linha única
+console.log('Linha 1');
+// comentário
+
+/*
+*Um bloco de código
+*Linha de comentário.
+*/
+
+console.log('Linha 2');
+console.log('Linha 3');
+console.log('Linha 4');
+// vai ser ignorado
+
+
+
---
+++ aula 6/index.js
@@ -0,0 +1,20 @@
+//let nome -> Declara a variável
+// = 'Henrique' -> Atribui o valor 'Henrique' à variável nome
+// let nome = 'Henrique' -> Declara e atribui o valor à variável nome
+// Não podemos criar variáveis com palavras reservadas. Exemplo: let, console, log
+// Variáveis precisam ter nomes significativos
+// Não podem começar com números. Exemplo: let 1nome (errado)
+// Não podem conter espaços ou traços. Exemplo: let nome-completo (errado)
+// Utilizamos camelCase. Exemplo: let nomeCompleto (certo)
+// Case-sensitive. Exemplo: let nomeCompleto e let NomeCompleto são variáveis diferentes
+// Não podemos redeclarar variáveis com let
+// NÃO UTILIZE VAR, UTILIZE LET.
+
+let nome = 'Henrique';
+
+
+console.log(nome, 'nasceu em 1984');
+console.log('Em 2000', nome, 'conheceu Maria.');
+console.log(nome, 'casou-se com Maria em 2012.');
+console.log('Maria teve 1 filho com', nome, 'em 2015.');
+console.log('O filho de', nome, 'se chama Eduardo.');
